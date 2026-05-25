d327a7dda33e17cfaa52791dd4c0c458cdd90bc9
Min <borellanas@gmail.com>
2026-05-24 23:52:03 -0400

    Add files via upload

---
+++ src/main/java/cl/duoc/usuarioev/UsuarioevApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.usuarioev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioevApplication.class, args);
+	}
+
+}
